ae8fe3123000bbf08ee6eb28efdf93c44df73f06
Andresjl389 <andresjaimes195@gmail.com>
2024-03-12 11:38:52 -0500

    contabilidad

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ContabilidadComponent } from '../app/contabilidad/contabilidad.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'contabilidad',
+        component: ContabilidadComponent
+    }
+];
