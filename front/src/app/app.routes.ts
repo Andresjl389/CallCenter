015d0982007a6e1909861efc032a40c5f10a9256
Andres Jaimes <131722154+Andresjl389@users.noreply.github.com>
2024-05-12 21:46:19 -0500

    Delete front directory

--- front/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ContabilidadComponent } from '../app/contabilidad/contabilidad.component';
-
-export const routes: Routes = [
-    {
-        path: 'contabilidad',
-        component: ContabilidadComponent
-    }
-];
